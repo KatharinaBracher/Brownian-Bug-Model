b697a94b076b1d9f7be4203dc375c0ddddcf0636
James Leadbetter <jsleadbetter1@gmail.com>
2024-04-01 16:52:05 +0100

    Updated model with correct output layer

--- pnn/job_scripts/qsub_histograms.sh
+++ pnn/job_scripts/qsub_histograms.sh
@@ -6,13 +6,14 @@
 # Directory in which to run code (-cwd or -wd <path-to-wd>)
 #$ -wd /home/s2603968/Brownian-Bug-Model
 # Requested runtime allowance
-#$ -l h_rt=00:20:00
+#$ -l h_rt=24:00:00
 # Requested memory (per core)
-#$ -l h_vmem=2G
+#$ -l h_vmem=64G
 # Requested number of cores in parallel environment
-#$ -pe sharedmem 8
+#$ -pe sharedmem 4
 # Email address for notifications
 #$ -M s2603968@ed.ac.uk
+#$ -m beas
 # Option to request resource reservation
 #$ -R y
 # Where to pipe the python output to.
